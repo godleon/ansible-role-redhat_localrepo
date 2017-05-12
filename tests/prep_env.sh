b4693eb0effef3fea0a2ed93b3554fd7937bf621
leon.tseng <leon.tseng@qct.io>
2017-05-12 15:22:31 +0800

    move subscription into script in testing code

---
+++ tests/prep_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+subscription-manager register --username='${RHN_USERNAME}' --password='${RHN_PASSWORD}'
+
+for pid in $(subscription-manager list --available | grep Pool | cut -d':' -f2 | tr -d ' \t'); do subscription-manager attach --pool=$pid; done
+
+subscription-manager repos --list | grep "Repo ID" | sort
+
+subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-openstack-10-rpms
+yum -y install ansible
+
+subscription-manager remove --all
+subscription-manager unregister
+subscription-manager clean
