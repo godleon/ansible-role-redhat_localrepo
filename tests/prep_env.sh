edb9d676a98590428603d9377dab9d3dd3082cc2
leon.tseng <leon.tseng@qct.io>
2017-05-15 10:31:21 +0800

    debug for ansible.cfg

--- tests/prep_env.sh
+++ tests/prep_env.sh
@@ -2,7 +2,7 @@
 
 for pid in $(subscription-manager list --available | grep Pool | cut -d':' -f2 | tr -d ' \t'); do subscription-manager attach --pool=$pid; done
 
-subscription-manager repos --list | grep "Repo ID" | sort
+#subscription-manager repos --list | grep "Repo ID" | sort
 
 subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-openstack-10-rpms
 yum -y install ansible
